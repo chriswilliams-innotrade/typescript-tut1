8f8c48f079c82b106bb1220f78d39542b8a46260
Chris Williams <chris@innotrade.io>
2018-07-05 23:06:17 -0600

    initial commit

---
+++ tstut.js
@@ -0,0 +1,6 @@
+var myName = "Chris";
+var myAge = 34;
+var canVote = true;
+var anything = "dog";
+anything = 2;
+document.getElementById("tsStuff").innerHTML = "My name is " + myName;
---
+++ tstut.ts
@@ -0,0 +1,7 @@
+var myName: string = "Chris";
+var myAge: number = 34;
+var canVote: boolean = true;
+var anything: any = "dog";
+anything = 2;
+
+document.getElementById("tsStuff").innerHTML = "My name is " + myName;
